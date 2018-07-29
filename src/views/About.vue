7c19fff6b376ca3fe111ddb6a830a654d3ed8f48
dawimuhammad <muhamadhaddawi@gmail.com>
2018-07-29 17:48:34 +0700

    Add feature logout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="about">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <Todolist/>
+    <v-toolbar>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
     </div>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down py-2">
+        <v-btn primary outline color="white" v-on:click="logout" style="height: 50px">
+            Logout
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <Todolist/>
   </div>
 </template>
 
@@ -19,6 +27,12 @@ export default {
     if (!localStorage.hasOwnProperty('token')) {
       this.$router.push('/')
     }
-  } 
+  },
+  methods : {
+    logout: function () {
+        localStorage.removeItem('token')
+        this.$router.push('/')
+    }
+  }
 }
 </script>
